896b57c4cd3d5654cbbc9530b2b82094bdc795ef
Abhishekh <abhiyb03@gmail.com>
2026-03-12 19:30:51 +0530

    removed url of axios in main .js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,7 +7,7 @@ import './style.css'
 const app = createApp(App)
 
 // ✅ Set Axios baseURL
-axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL
+//axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL
 
 // ✅ Register Axios globally
 app.config.globalProperties.$axios = axios
